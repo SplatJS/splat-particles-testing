ffd1b2e4ab8894be03ff7f7f10d6842575ebfc1f
Alex Bezuska <abezuska@gmail.com>
2016-04-03 18:30:00 -0400

    fix particle defaults

--- src/scripts/particle-fire.js
+++ src/scripts/particle-fire.js
@@ -6,20 +6,34 @@ module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
 
   var fire = new particles.Config();
   fire.origin = {
-    "x": 350,
-    "y": 300
+    "x": middleCenterX(game, entity),
+    "y": middleCenterY(game, entity)
   };
   fire.prefab = "fire";
   fire.qtyMin = 0;
   fire.qtyMax = 12;
-  fire.angle =  Math.PI / 2;
+  //fire.angle =  Math.PI / 2;
   fire.arcWidth = Math.PI  / 8;
   fire.sizeMin = 0.5;
   fire.sizeMax = 2;
   fire.velocityMin = 0.9;
   fire.velocityMax = 0.5;
-  fire.lifeSpan = 500;
+  //fire.lifeSpan = 500;
   particles.create(game, fire);
 
 
 };
+
+
+
+function middleCenterX(game, entity) {
+  var position = game.entities.get(entity, "position");
+  var size = game.entities.get(entity, "size");
+  return position.x + (size.width / 2);
+}
+
+function middleCenterY(game, entity) {
+  var position = game.entities.get(entity, "position");
+  var size = game.entities.get(entity, "size");
+  return position.y + (size.height / 2);
+}
