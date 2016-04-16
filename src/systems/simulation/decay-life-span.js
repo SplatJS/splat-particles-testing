6123ab31d618a1c9e02b41d36f2dedefc1edd3cb
Eric Lathrop <eric@ericlathrop.com>
2016-04-16 11:08:45 -0400

    Rename system

--- src/systems/simulation/decay-life-span.js
+++ src/systems/simulation/decay-life-span.js
@@ -3,11 +3,9 @@
 module.exports = function(ecs, game) {
   ecs.addEach(function decayLifeSpan(entity, elapsed) {
     var lifeSpan = game.entities.get(entity, "lifeSpan");
-
     lifeSpan.current += elapsed;
     if (lifeSpan.current >= lifeSpan.max) {
       game.entities.destroy(entity);
     }
-
   }, "lifeSpan");
 };
